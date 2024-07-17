d76623f33cc33b8045eed41e1f381b48c6992e9e
Alex <alex.tec.pessoal@gmail.com>
2024-07-17 20:58:41 -0300

    commit

---
+++ src/main/java/com/exeptions/exeptions/ExeptionsApplication.java
@@ -0,0 +1,13 @@
+package com.exeptions.exeptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExeptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exeptions/exeptions/ExeptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exeptions.exeptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
